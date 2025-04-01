3b987d8a4a21f12adc555bf3f3568f2241ce1dd9
Manasa1-2 <manasaparampogu123@gmail.com>
2025-04-01 17:40:49 +0530

    32-commit

--- src/main/lab.py
+++ src/main/lab.py
@@ -1,22 +1,9 @@
 # lab.py
 
-# Create a function by following these instructions
-
-"""
-#Function Name: The function should be named count_vowels.
-
-#Parameters: The function should take one parameter named string, 
-                which represents the input string.
-
-#Functionality: The function should count the number of vowels 
-                (both lowercase and uppercase) in the input string and return the count.
-
-#Implementation: Inside the function, use a loop to iterate through 
-                each character of the input string. Check if each character is a vowel 
-                (either lowercase or uppercase) by comparing it to a string containing 
-                all vowels ("aeiouAEIOU"). If a character is found in the vowels string, 
-                increment a counter variable by 1. Finally, return the value of the counter 
-                variable as the result.
-
-"""
-
+def count_vowels(string):
+    vowels = "aeiouAEIOU"
+    c = 0
+    for char in string:
+        if char in vowels:
+            c += 1
+    return c
